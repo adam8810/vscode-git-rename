927d0406413cd03e18e3652ecbb002d13b484fd0
Adam Booth <adam@ambooth.com>
2020-01-17 14:22:29 -0600

    Initial working version MVP

---
+++ README.md
@@ -0,0 +1,16 @@
+# git-rename README
+
+## Features
+
+Uses git-mv to rename files
+https://git-scm.com/docs/git-mv
+
+## Requirements
+
+You must have git installed
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of git-rename
---
+++ src/extension.ts
@@ -0,0 +1,64 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+const spawnCMD = require("spawn-command");
+const treeKill = require("tree-kill");
+
+let process: any = null;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  let channel = vscode.window.createOutputChannel("git-rename");
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "git-rename" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  // The code you place here will be executed every time your command is executed
+  let disposable = vscode.commands.registerCommand(
+    "extension.git-rename",
+    file => {
+      console.log("h");
+      let folderPath = vscode.workspace.rootPath;
+
+      function log(str: string) {
+        //Write to output.
+        console.log(str);
+        channel.appendLine(str);
+      }
+
+      const regex = /\.js/gi;
+      const command = `git mv ${file.fsPath} ${file.fsPath.replace(
+        regex,
+        ".ts"
+      )}`;
+      log(command);
+
+      process = spawnCMD(command, { cwd: folderPath });
+      process.stdout.on("data", (buffer: Buffer) => log(buffer.toString()));
+
+      process.stderr.on("data", (err: Buffer) => {
+        const str = err.toString();
+        log(str);
+        vscode.window.showErrorMessage(str);
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+  if (process) {
+    treeKill(process.pid, "SIGTERM", function(err: any) {
+      if (err) {
+        treeKill(process.pid, "SIGKILL");
+      }
+      process = null;
+    });
+  }
+}
