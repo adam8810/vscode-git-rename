2b1c03dec78e09ddc6f25e259059459916bcff94
Jan Dolejsi <JDolejsi@slb.com>
2022-06-11 20:49:48 +0200

    - tslint -> eslint - dependencies - spawn -> terminal - relative file path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
-{
-	"rules": {
-		"no-string-throw": true,
-		"no-unused-expression": true,
-		"no-duplicate-variable": true,
-		"curly": true,
-		"class-name": true,
-		"semicolon": [
-			true,
-			"always"
-		],
-		"triple-equals": true
+module.exports = {
+	parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
+	extends: [
+	  'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+	],
+	parserOptions: {
+	  ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+	  sourceType: 'module',  // Allows for the use of imports
 	},
-	"defaultSeverity": "warning"
-}
+	rules: {
+	  // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+	  // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+	  "@typescript-eslint/semi": "warn",
+	  "curly": "warn",
+	  "eqeqeq": "warn",
+	  "no-throw-literal": "warn",
+	  "semi": "off"
+	},
+	ignorePatterns: ["out/", "dist/", "node_modules/"]
+  };
--- src/extension.ts
+++ src/extension.ts
@@ -1,72 +1,97 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from 'path';
 import logger from "./logger";
-const spawnCMD = require("spawn-command");
-const treeKill = require("tree-kill");
 
-let process: any = null;
+
+let terminal: vscode.Terminal | undefined = undefined;
+let channel: vscode.OutputChannel | undefined = undefined;
+
+const TERMINAL_NAME = "Git rename";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  let channel = vscode.window.createOutputChannel("git-rename");
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "git-rename" is now active!');
+  console.log('Extension "git-rename" is now active.');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   // The code you place here will be executed every time your command is executed
-  let disposable = vscode.commands.registerCommand(
+  context.subscriptions.push(vscode.commands.registerCommand(
     "extension.git-rename",
-    async file => {
-      const log = logger(channel);
-      let cwd;
-      const folders = vscode.workspace.workspaceFolders;
+    async file => gitMove(file).catch(err => vscode.window.showErrorMessage(err.message ?? err))
+  ));
 
-      if (folders && folders[0]) {
-        cwd = folders[0].uri.path;
-      }
-      else return;
-
-
-      const dialogOptions: vscode.InputBoxOptions = {
-        value: file.fsPath
-      };
-
-      const newPath = await vscode.window.showInputBox(dialogOptions);
-
-      if (newPath == null || newPath === "") {
-        return;
-      }
-
-      const command = `git mv "${file.fsPath}" "${newPath}"`;
-      log(command);
-
-      process = spawnCMD(command, { cwd });
-      process.stdout.on("data", (buffer: Buffer) => log(buffer.toString()));
-
-      process.stderr.on("data", (err: Buffer) => {
-        const str = err.toString();
-        log(str);
-        vscode.window.showErrorMessage(str);
-      });
+  context.subscriptions.push(vscode.window.onDidCloseTerminal(closedTerminal => {
+    if (closedTerminal.processId === terminal?.processId) {
+      // clear the cached terminal reference
+      terminal = undefined;
     }
-  );
+  }));
 
-  context.subscriptions.push(disposable);
+  // was the terminal window created in the previous session? reuse it!
+  terminal = vscode.window.terminals
+    .find(previousTerminal => previousTerminal.name === TERMINAL_NAME);
+}
+
+async function gitMove(file: vscode.Uri): Promise<void> {
+  channel = channel ?? vscode.window.createOutputChannel("git-rename");
+  const log = logger(channel);
+
+  const wsFolder = vscode.workspace.getWorkspaceFolder(file);
+  if (!wsFolder) {
+    vscode.window.showErrorMessage('Selected file is not in a VS Code workspace, or no workspace folder is open.');
+    return;
+  }
+
+  const relativePath = path.relative(wsFolder.uri.fsPath, file.fsPath);
+
+  const newRelativePath = await vscode.window.showInputBox({
+    value: relativePath
+  });
+
+  if (!newRelativePath || newRelativePath === "") {
+    // canceled by user
+    return;
+  }
+
+  const newPath = vscode.Uri.joinPath(wsFolder.uri, newRelativePath);
+
+  const gitPath = getGitPath();
+  const command = `${gitPath} mv "${file.fsPath}" "${newPath.fsPath}"`;
+  log(command);
+
+  terminal = terminal ?? vscode.window.createTerminal({
+    cwd: wsFolder.uri.fsPath,
+    name: TERMINAL_NAME
+  });
+
+  terminal.sendText(command);
+  terminal.show(true);
+}
+
+function getGitPath(): string {
+  const configuredGitPaths = vscode.workspace.getConfiguration().get<string | string[] | undefined>("git.path");
+
+  if (!configuredGitPaths) {
+    // assume it is in the environment _path_
+    return "git";
+  } else if (typeof configuredGitPaths === "string") {
+    return configuredGitPaths;
+  } else if (Array.isArray(configuredGitPaths)) {
+    return configuredGitPaths[0];
+  } else {
+    vscode.window.showWarningMessage(`Configured git.path ${configuredGitPaths} is not supported. Using 'git'.`);
+    return "git";
+  }
 }
 
 // this method is called when your extension is deactivated
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {
-  if (process) {
-    treeKill(process.pid, "SIGTERM", function (err: any) {
-      if (err) {
-        treeKill(process.pid, "SIGKILL");
-      }
-      process = null;
-    });
-  }
+  // no need to kill the process as VS Code will kill the Terminal
 }
